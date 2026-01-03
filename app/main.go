93a0883059f34af31d1bc4ed5098532ddd51b79a
agkmw <185657819+agkmw@users.noreply.github.com>
2026-01-03 20:25:23 +0630

    implement echo

--- app/main.go
+++ app/main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
+	"strings"
 )
 
 const (
 	EXIT = "exit"
+	ECHO = "echo"
 )
 
 func main() {
@@ -15,16 +18,33 @@ func main() {
 		fmt.Print("$ ")
 
 		r := bufio.NewReader(os.Stdin)
-		rawCmd, err := r.ReadString('\n')
-		cmd := rawCmd[:len(rawCmd)-1]
+		input, err := r.ReadString('\n')
 		if err != nil {
 			fmt.Println(err)
 		}
 
-		if cmd == EXIT {
-			return
-		}
+		fields := strings.Fields(input)
 
-		fmt.Printf("%s: command not found\n", cmd)
+		cmd := fields[0]
+		args := fields[1:]
+
+		switch cmd {
+		case EXIT:
+			return
+
+		case ECHO:
+			var buf bytes.Buffer
+			for i, arg := range args {
+				buf.WriteString(arg)
+				if i != len(args)-1 {
+					buf.WriteString(" ")
+				}
+			}
+			fmt.Println(buf.String())
+			continue
+
+		default:
+			fmt.Printf("%s: command not found\n", cmd)
+		}
 	}
 }
