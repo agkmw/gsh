837cf638061b1e113833a3be2ec75e97174e4ab9
agkmw <185657819+agkmw@users.noreply.github.com>
2026-01-03 22:21:13 +0630

    locate executable files

--- app/main.go
+++ app/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"os"
+	"path"
+	"path/filepath"
 	"slices"
 	"strings"
 )
@@ -49,11 +52,34 @@ func main() {
 
 		case TYPE:
 			for _, arg := range args {
-				if !slices.Contains(builtins, arg) {
-					fmt.Printf("%s: not found\n", arg)
-				} else {
+				if slices.Contains(builtins, arg) {
 					fmt.Printf("%s is a shell builtin\n", arg)
+					continue
 				}
+
+				found := false
+				rawPath := os.Getenv("PATH")
+				paths := strings.Split(rawPath, ":")
+				for _, p := range paths {
+					// fmt.Println("debug: path: ", p)
+					f, err := handlePath(arg, p)
+					if err != nil {
+						fmt.Printf("failed to handle path: %s", err)
+						continue
+					}
+
+					if f {
+						fmt.Printf("%s is %s\n", arg, path.Join(p, arg))
+						found = true
+						break
+					}
+				}
+
+				if !found {
+					fmt.Printf("%s: not found\n", arg)
+				}
+
+				continue
 			}
 			continue
 
@@ -61,4 +87,56 @@ func main() {
 			fmt.Printf("%s: command not found\n", cmd)
 		}
 	}
+}
+
+func handlePath(target string, path string) (bool, error) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return false, nil
+		}
+		return false, fmt.Errorf("failed to read dir: %s\n", err)
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() {
+			absPath, err := filepath.Abs(entry.Name())
+			if err != nil {
+				fmt.Printf("failed to get abs path: %s\n", entry.Name())
+				continue
+			}
+
+			found, err := handlePath(target, absPath)
+			if err != nil {
+				fmt.Printf("failed to handle path: %s", err)
+				continue
+			}
+
+			if found {
+				return true, nil
+			}
+
+			continue
+		}
+
+		info, err := entry.Info()
+		if err != nil {
+			fmt.Printf("failed to get entry info: %s", err)
+			continue
+		}
+
+		perm := info.Mode().Perm()
+		if !strings.Contains(perm.String(), "x") {
+			continue
+		}
+
+		// fmt.Printf("ENTRY: %s | PERM: %s\n", entry.Name(), perm)
+
+		if target == entry.Name() {
+			// fmt.Printf("found %s\n", target)
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
