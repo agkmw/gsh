4cbf68d9ed680ce1fe5ee848ee105924f0dfd0a6
agkmw <185657819+agkmw@users.noreply.github.com>
2026-01-03 20:01:36 +0630

    add repl

--- app/main.go
+++ app/main.go
@@ -7,13 +7,15 @@ import (
 )
 
 func main() {
-	fmt.Print("$ ")
+	for {
+		fmt.Print("$ ")
 
-	r := bufio.NewReader(os.Stdin)
-	cmd, err := r.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
+		r := bufio.NewReader(os.Stdin)
+		cmd, err := r.ReadString('\n')
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Printf("%s: command not found\n", cmd[:len(cmd)-1])
 	}
-
-	fmt.Printf("%s: command not found\n", cmd[:len(cmd)-1])
 }
