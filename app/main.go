ed744055e2f78f70742b680f12095a8975ab20e4
agkmw <185657819+agkmw@users.noreply.github.com>
2026-01-03 19:58:19 +0630

    read commands from the stdin

--- app/main.go
+++ app/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
-
 
 func main() {
 	fmt.Print("$ ")
+
+	r := bufio.NewReader(os.Stdin)
+	cmd, err := r.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("%s: command not found\n", cmd[:len(cmd)-1])
 }
